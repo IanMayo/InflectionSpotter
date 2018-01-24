98c59732b3c4fcf33e4fc9768c8cab335b0e077a
Ian Mayo <ian@planetmayo.com>
2018-01-24 12:55:15 +0000

    refactor, to tidy

--- src/FourParameterLogisticSecondDrivative.java
+++ src/FourParameterLogisticSecondDrivative.java
@@ -1,19 +1,27 @@
 import org.apache.commons.math3.analysis.UnivariateFunction;
 
-public class FourParameterLogisticSecondDrivative implements UnivariateFunction {
+public class FourParameterLogisticSecondDrivative implements UnivariateFunction
+{
 
-	public double[] coeff;
-	
-	@Override
-	public double value(double x) {
-		double a = coeff[0];
-		double b = coeff[1];
-		double c = coeff[2];
-		double d = coeff[3];
-		double cb = Math.pow(c, b);
-		double xb = Math.pow(x, b);
-		return -(b*cb*Math.pow(x, b-2)*(b*cb-cb-b*xb-xb)*(a-d))/Math.pow(cb+xb,3);
-		
-	}
+  final public double[] coeff;
+
+  public FourParameterLogisticSecondDrivative(final double[] modelParameters)
+  {
+    coeff = modelParameters;
+  }
+
+  @Override
+  public double value(final double x)
+  {
+    final double a = coeff[0];
+    final double b = coeff[1];
+    final double c = coeff[2];
+    final double d = coeff[3];
+    final double cb = Math.pow(c, b);
+    final double xb = Math.pow(x, b);
+    return -(b * cb * Math.pow(x, b - 2) * (b * cb - cb - b * xb - xb) * (a - d))
+        / Math.pow(cb + xb, 3);
+
+  }
 
 }
